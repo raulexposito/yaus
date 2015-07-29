c62b01ceee93e56386439dbe08a7fc0eaff0bb9d
Raúl Expósito <raul@raulexposito.com>
2015-07-29 18:32:57 +0200

    Added implementation and tests for the Visit pojo

--- persistence/src/main/java/com/raulexposito/yaus/persistence/dao/Visit.java
+++ persistence/src/main/java/com/raulexposito/yaus/persistence/dao/Visit.java
@@ -1,8 +1,9 @@
 package com.raulexposito.yaus.persistence.dao;
 
-import lombok.Data;
+import lombok.Getter;
+import org.joda.time.LocalDateTime;
 
-@Data
+@Getter
 public class Visit {
 
     private Integer year;
@@ -24,7 +25,27 @@ public class Visit {
     private String userAgent;
 
     public Visit (final String ip, final String userAgent) {
+        generate(ip, userAgent, new LocalDateTime());
+    }
+
+    // ---------------------------------------------
+    // package protected
+    // just for testing purposes
+    // ---------------------------------------------
+
+    Visit (final String ip, final String userAgent, final LocalDateTime localDateTime) {
+        generate(ip, userAgent, localDateTime);
+    }
+
+    void generate (final String ip, final String userAgent, final LocalDateTime localDateTime) {
         this.ip = ip;
         this.userAgent = userAgent;
+        year = localDateTime.getYear();
+        month = localDateTime.getMonthOfYear();
+        day = localDateTime.getDayOfMonth();
+        hour = localDateTime.getHourOfDay();
+        minute = localDateTime.getMinuteOfHour();
+        second = localDateTime.getSecondOfMinute();
+        weekday = localDateTime.getDayOfWeek();
     }
 }
---
+++ persistence/src/test/java/com/raulexposito/yaus/persistence/dao/VisitTest.java
@@ -0,0 +1,36 @@
+package com.raulexposito.yaus.persistence.dao;
+
+import org.joda.time.DateTimeConstants;
+import org.joda.time.LocalDateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VisitTest {
+
+    @Test
+    public void theIPIsStored () {
+        final Visit visit = new Visit("127.0.0.1", "Mozilla/5.0");
+        Assert.assertEquals("127.0.0.1", visit.getIp());
+    }
+
+    @Test
+    public void theUserAgentIsStored () {
+        final Visit visit = new Visit("127.0.0.1", "Mozilla/5.0");
+        Assert.assertEquals("Mozilla/5.0", visit.getUserAgent());
+    }
+
+    @Test
+    // Thu, 01 Jan 2015 12:30:00 GMT
+    public void theDateIsStored () {
+        final LocalDateTime localDateTime = new LocalDateTime(2015,
+                DateTimeConstants.JANUARY, 1, 12, 30, 0);
+        final Visit visit = new Visit("127.0.0.1", "Mozilla/5.0", localDateTime);
+        Assert.assertEquals(visit.getYear(), new Integer(2015));
+        Assert.assertSame(visit.getMonth(), DateTimeConstants.JANUARY);
+        Assert.assertSame(visit.getDay(), 1);
+        Assert.assertSame(visit.getHour(), 12);
+        Assert.assertSame(visit.getMinute(), 30);
+        Assert.assertSame(visit.getSecond(), 0);
+        Assert.assertSame(visit.getWeekday(), DateTimeConstants.THURSDAY);
+    }
+}
